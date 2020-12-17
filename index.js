69ee878f7e557da32554a5735abeadb117546017
Dongjoon Lee <vocolor@hanmail.net>
2020-12-18 01:01:29 +0900

    Protect personal info

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { user, User } = require("./models/User");
+
+//application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dongjoonlee:ehdwns82@boiler-plate.xf4bs.mongodb.net/test', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
-}).then(() => console.log(',Mongodb connected...'))
+}).then(() => console.log('Mongodb connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Oh yeah')
+  res.send('Hello World!')
 })
+
+app.post('/register', (req, res) => {
+
+  // For sign-up, when server can bring the datas from client, put those datas into database.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
